0afa1df2c95363af7300046a9d259cf3537e2686
Raffay Sheikh <raffaysheikh64@gmail.com>
2026-05-23 16:16:19 +0500

    New code

---
+++ code.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class teacher{
+    public:
+    string name;
+    void display(){
+        cout << "Teacher Name: " << name << endl;
+    }
+};
+
+class course{
+    public:
+    string coursename;
+    void display(){
+        cout << "Course Name: " << coursename << endl;
+    }
+};
+int main(){
+    teacher t1;
+    course c1;
+    teacher *t2;
+    course *c2;
+    t2 = &t1;
+    c2 = &c1;
+    t1.name = "Dostoevesky";
+    c1.coursename = "Literature";
+    t1.display();
+    c1.display();
+    t2->display();
+    c2->display();
+
+}
--- overloading.cpp
+++ overloading.cpp
@@ -6,7 +6,6 @@ public:
     double val;
     Number(double v) { val = v; }
     Number operator + (Number obj) { return Number(val + obj.val); }
-    Number operator + (Number obj) {return Number(val * obj.val);}
     Number operator - (Number obj) { return Number(val - obj.val); }
     Number operator * (Number obj) { return Number(val * obj.val); }
     Number operator / (Number obj) { return Number(val / obj.val); }
@@ -16,12 +15,14 @@ int main() {
     Number n1(10), n2(2);
 
     Number a = n1 + n2;
-    Number a2 = n1 + n2;
     Number b = n1 - n2;
     Number c = n1 * n2;
     Number d = n1 / n2;
 
     cout << "Add: " << a.val << endl;
+    cout << "Subtract: " << b.val << endl;
+    cout << "Multiply: " << c.val << endl;
+    cout << "Divide: " << d.val << endl;
 
     return 0;
 }
